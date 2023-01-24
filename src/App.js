220bbacbc207be3859215137985b8a78b9cb7dae
Amol shelke <shelkeamol897@gmail.com>
2023-01-24 22:35:43 +0530

    Added icons, useState and useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 
-function App() {
+// import axios
+import axios from "axios";
+
+// import icons
+import {
+  IoMdSunny,
+  IoMdRainy,
+  IoMdCloudy,
+  IoMdSnow,
+  IoMdThunderstorm,
+  IoMdSearch,
+} from "react-icons/io";
+
+import {
+  BsCloudHaze2Fill,
+  BsCloudDrizzleFill,
+  BsEye,
+  BsWater,
+  BsThermometer,
+  BsWind,
+} from "react-icons/bs";
+
+import { TbTemperatureCelsius } from "react-icons/tb";
+import { ImSpinner8 } from "react-icons/im";
+
+// API key
+const APIkey = `92e5fe71edba4a5e83a4adceb8e724df`;
+
+const App = () => {
   return (
     <div className="App">
       <p>Creating Weather App</p>
     </div>
   );
-}
+};
 
 export default App;
