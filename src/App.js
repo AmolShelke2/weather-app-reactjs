0eca464f2c2db65a0cfcf3a760f3cbfa8016917d
Amol shelke <shelkeamol897@gmail.com>
2023-02-02 18:50:19 +0530

    Adding weather input logic

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ const APIkey = `92e5fe71edba4a5e83a4adceb8e724df`;
 const App = () => {
   const [data, setData] = useState(null);
   const [location, setLocation] = useState('Nagpur');
+  const [weatherInput, setWeatherInput] = useState('');
 
   // Fetch the weather data
 
